6c5f4b07cecb9545557fbeb7d2ea455723882c50
Taylor Davidson <taylordavidson@ymail.com>
2018-03-13 22:39:39 -0700

    init

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$db['db_host']= "localhost";
+$db['db_user']= "root";
+$db['db_pass']= "root";
+$db['db_name']= "cms";
+
+foreach($db as $k=>$v){
+    define(strtoupper($k), $v);
+}
+
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection){
+    echo "We are connected";
+}
+
+
+
+
+?>
