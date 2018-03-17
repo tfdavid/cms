41af30fa61254f500986de9f5a11c85dfa3b45c3
Taylor Davidson <taylordavidson@ymail.com>
2018-03-16 20:21:53 -0700

    create new post done

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,4 +1,41 @@
+
+
+<?php
+    if(isset($_POST['create_post'])){
+        
+        $post_title = $_POST['title'];
+        $post_author = $_POST['author'];
+        $post_category_id = $_POST['post_category_id'];
+        $post_status = $_POST['post_status'];
+
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']["tmp_name"];
+
+        $post_tags = $_POST['post_tags'];
+        $post_content = $_POST['post_content'];
+        $post_date = date('d-m-y');
+        $post_comment_count = 4;
+
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+
+
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author,
+        post_date, post_image, post_content, post_tags, post_comment_count,post_status) ";
+        $query.= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}',
+        '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}') ";  
+
+        $create_post_query = mysqli_query($connection, $query);
+
+        confirmQuery($create_post_query);
+       
+    }
+?>
+
+
+
+
 <form action="" method="post" enctype="multipart/form-data">
+
     <div class="form-group">
         <label for="title">Post Title</label>
         <input type="text" class="form-control" name="title">
