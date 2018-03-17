391183f82f257b7685dcd18b0f4d809d6521168e
Taylor Davidson <taylordavidson@ymail.com>
2018-03-17 03:49:51 -0700

    main posts admin done with base functionality

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -41,9 +41,25 @@
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-        <label for="post_category_id">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
-    </div>
+        <select name="post_category_id" id="">
+            <?php
+                $query = "SELECT * FROM categories";
+                $select_categories= mysqli_query($connection, $query);
+
+                confirmQuery($select_categories);
+
+                while($row = mysqli_fetch_assoc($select_categories)){
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+
+                    echo "<option value='$cat_id'>{$cat_title}</option>";
+                }
+
+            ?>
+        
+        
+        </select>
+     </div>
      <div class="form-group">
         <label for="author">Post Author</label>
         <input type="text" class="form-control" name="author">
--- index.php
+++ index.php
@@ -20,11 +20,12 @@
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
                 $post_image = $row['post_image'];
-                $post_content = $row['post_content'];
+                $post_content = substr($row['post_content'], 0, 100);
 
             ?>
                 <h1 class="page-header">
@@ -34,14 +35,14 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
-                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <img width=200 class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
