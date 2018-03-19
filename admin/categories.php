8ce2393e5332e21313b69b63d1267a3c26c07027
Taylor Davidson <taylordavidson@ymail.com>
2018-03-18 22:21:22 -0700

    edited html

--- admin/categories.php
+++ admin/categories.php
@@ -17,6 +17,7 @@
 
                 <!-- Page Heading -->
                 <div class="row">
+                
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to Admin
