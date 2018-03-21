45d81b932d96beedddeb2b79bbf4f81cfe42c160
Taylor Davidson <taylordavidson@ymail.com>
2018-03-20 23:56:47 -0700

    spacing refactor

--- admin/categories.php
+++ admin/categories.php
@@ -8,16 +8,12 @@
 
 
 
-
-
-
         <div id="page-wrapper">
 
             <div class="container-fluid">
 
                 <!-- Page Heading -->
                 <div class="row">
-                
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to Admin
