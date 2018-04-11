070d3b003ae1be5f804a051d19fb34763d5a367a
Taylor Davidson <taylordavidson@ymail.com>
2018-04-11 04:46:05 -0700

    filter posts and comments based on approved or published(display)

--- index.php
+++ index.php
@@ -15,39 +15,53 @@
 
             <?php
 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts WHERE post_status = 'published' ";
             $select_all_posts_query = mysqli_query($connection, $query);
+            // if(mysqli_error())
+            if (mysqli_num_rows($select_all_posts_query)==0) { echo "<h1 class='text-center'>No Content Available</h1>"; }
+            else{
 
-            while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_content = substr($row['post_content'], 0, 100);
+            
+                while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                    // print_r($row);
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = substr($row['post_content'], 0, 100);
+                    $post_status = $row['post_status'];
+                    // if($post_status !== 'published'){
+                    //     echo "<h1 class='text-center'>No Content Available</h1>";
+                    // }
+                    // else{
+
+                
+                ?>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                    <hr>
+                        <img width=200 class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>  
+
+
+                <?php }   
+                } 
             ?>
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-                <hr>
-                    <img width=200 class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>  
-
-
-            <?php } ?>
 
                  
             </div>
